6a54b1ae4514c32503d761041f53cd28e3e11671
unknown <nguyenphiikhanh@gmail.com>
2021-08-05 19:09:39 +0700

    complete dashboard

--- resources/views/admin/dashboard/partials/header.blade.php
+++ resources/views/admin/dashboard/partials/header.blade.php
@@ -1,22 +1,16 @@
-    <!-- Header -->
-    <!-- Header -->
+
     <div class="header bg-primary pb-6">
         <div class="container-fluid">
           <div class="header-body">
             <div class="row align-items-center py-4">
               <div class="col-lg-6 col-7">
-                <h6 class="h2 text-white d-inline-block mb-0">Default</h6>
                 <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                   <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                     <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
                     <li class="breadcrumb-item"><a href="#">Dashboards</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Default</li>
+                    <li class="breadcrumb-item active" aria-current="page">Trang chủ</li>
                   </ol>
                 </nav>
-              </div>
-              <div class="col-lg-6 col-5 text-right">
-                <a href="#" class="btn btn-sm btn-neutral">New</a>
-                <a href="#" class="btn btn-sm btn-neutral">Filters</a>
               </div>
             </div>
             <!-- Card stats -->
@@ -27,19 +21,16 @@
                   <div class="card-body">
                     <div class="row">
                       <div class="col">
-                        <h5 class="card-title text-uppercase text-muted mb-0">Total traffic</h5>
+                        <h5 class="card-title text-uppercase text-muted mb-0">Bài viết trên Blog</h5>
                         <span class="h2 font-weight-bold mb-0">350,897</span>
+                        <span class="text-nowrap"> bài viết</span>
                       </div>
                       <div class="col-auto">
                         <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
-                          <i class="ni ni-active-40"></i>
+                          <i class="ni ni-book-bookmark"></i>
                         </div>
                       </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                      <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                      <span class="text-nowrap">Since last month</span>
-                    </p>
                   </div>
                 </div>
               </div>
@@ -49,19 +40,16 @@
                   <div class="card-body">
                     <div class="row">
                       <div class="col">
-                        <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
+                        <h5 class="card-title text-uppercase text-muted mb-0">Thành viên trên blog</h5>
                         <span class="h2 font-weight-bold mb-0">2,356</span>
+                        <span class="text-nowrap"> thành viên</span>
                       </div>
                       <div class="col-auto">
                         <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
-                          <i class="ni ni-chart-pie-35"></i>
+                          <i class="ni ni-circle-08"></i>
                         </div>
                       </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                      <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                      <span class="text-nowrap">Since last month</span>
-                    </p>
                   </div>
                 </div>
               </div>
@@ -71,19 +59,16 @@
                   <div class="card-body">
                     <div class="row">
                       <div class="col">
-                        <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
+                        <h5 class="card-title text-uppercase text-muted mb-0">Danh mục bài viết</h5>
                         <span class="h2 font-weight-bold mb-0">924</span>
+                        <span class="text-nowrap"> danh mục</span>
                       </div>
                       <div class="col-auto">
                         <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
-                          <i class="ni ni-money-coins"></i>
+                          <i class="ni ni-map-big"></i>
                         </div>
                       </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                      <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                      <span class="text-nowrap">Since last month</span>
-                    </p>
                   </div>
                 </div>
               </div>
@@ -93,19 +78,16 @@
                   <div class="card-body">
                     <div class="row">
                       <div class="col">
-                        <h5 class="card-title text-uppercase text-muted mb-0">Performance</h5>
+                        <h5 class="card-title text-uppercase text-muted mb-0">Lượt xem trên Blog</h5>
                         <span class="h2 font-weight-bold mb-0">49,65%</span>
+                        <span class="text-nowrap"> Lượt xem</span>
                       </div>
                       <div class="col-auto">
                         <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
-                          <i class="ni ni-chart-bar-32"></i>
+                          <i class="ni ni-satisfied"></i>
                         </div>
                       </div>
                     </div>
-                    <p class="mt-3 mb-0 text-sm">
-                      <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                      <span class="text-nowrap">Since last month</span>
-                    </p>
                   </div>
                 </div>
               </div>
